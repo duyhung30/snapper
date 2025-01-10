a5fa58f7a29de794caa9a72584096f861236703b
duyhung30 <buivuduyhung2003@gmail.com>
2025-01-10 17:00:49 +0700

    add image for onboarding screen

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,19 @@
 import { Link, router } from 'expo-router'
-import { Text, View, SafeAreaView, TouchableOpacity } from 'react-native'
+import {
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native'
+// import { Image } from 'react-native'
+import { Image } from 'expo-image'
 import CustomButton from '@/components/CustomButton'
 
+const { width: windowWidth, height: windowHeight } = Dimensions.get('window')
 const Welcome = () => {
   return (
-    <SafeAreaView className='flex-1 items-center justify-end space-y-6 bg-background-default' >
+    <SafeAreaView className='flex-1 items-center justify-end space-y-6 bg-background-default'>
       {/* <View className='w-1/2'>
         <Link href={'/(auth)/sign-up'} asChild>
           <TouchableOpacity className='rounded-full items-center p-3 bg-blue-300'>
@@ -20,15 +29,37 @@ const Welcome = () => {
           </TouchableOpacity>
         </Link>
       </View> */}
-      < CustomButton
+
+      <View className='w-24 h-24 bg-gray-800 rounded-lg flex items-center justify-center'>
+        <Image
+          source={require('../../assets/images/friends.jpg')}
+          // resizeMode='cover'
+          transition={1000}
+          cachePolicy='memory-disk' // Enable caching
+          style={{
+            width: windowWidth,
+            height: windowHeight,
+            // borderRadius: 30,
+          }}
+        />
+      </View>
+      <Text className='text-secondary-200 text-xl text-center mt-2 mx-8 font-JakartaSemiBold'>
+        Live pics from your friends, on your home screen
+      </Text>
+
+      <CustomButton
         title={'Sign up'}
-        onPress={() => { router.push('/(auth)/sign-up') }}
+        onPress={() => {
+          router.push('/(auth)/sign-up')
+        }}
         className='items-center w-5/6'
       />
 
       <CustomButton
         title={'Sign In'}
-        onPress={() => { router.push('/(auth)/sign-in') }}
+        onPress={() => {
+          router.push('/(auth)/sign-in')
+        }}
         className='items-center bg-secondary-600 w-5/6 mb-14'
       />
     </SafeAreaView>
